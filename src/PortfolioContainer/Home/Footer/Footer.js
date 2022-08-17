d9afad25e8c9d692f8432b00d8b497b293209256
mahant1598shivam@gmail.com <mahant1598shivam@gmail.com>
2022-08-16 23:03:33 -0700

    header done

---
+++ src/PortfolioContainer/Home/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div class="footer-wrapper">
+      <div class="wrapper">
+        <div class="wave"></div>
+      </div>
+    </div>
+  );
+}
---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Typical from "react-typical";
+import "./Profile.css";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+            <a href="#">
+              <i className="fa fa-github"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-linkedin-square"></i>
+            </a>
+          </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text"> </span>
+            Hello, I'M <span className="highlighted-text">Shivam</span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev 🥇",
+                    1500,
+                    "Front End Dev 🖥️",
+                    1500,
+                    "React & Vue Dev 💟",
+                    1500,
+                    "Sitecore Dev 🔯",
+                    1500,
+                  ]}
+                />
+              </h1>
+              <span>knack of building applications with fron and back end operations</span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">
+              {" "}
+              Hire Me {" "}
+            </button>
+            <a href="ehizcv.pdf" download="Shivam ehizcv.pdf" title="Opens in a new window" target="_black">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+            <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
