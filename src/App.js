dba5c6f965fa6372c2df74646778cfd6ec00db43
mahant1598shivam@gmail.com <mahant1598shivam@gmail.com>
2022-08-19 15:52:38 -0700

    global styling and structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import Profile from "./PortfolioContainer/Home/Profile";
-import Footer  from "./PortfolioContainer/Home/Footer/Footer";
+import Wave  from "./PortfolioContainer/Home/WaveFolder/Wave";
+import Nav  from "./PortfolioContainer/Nav/Nav";
+import Contact from "./PortfolioContainer/ContactMe/Contact";
+import About from "./PortfolioContainer/AboutMe/About";
+import Experience from "./PortfolioContainer/Experience/Experince"
+import Footer from "./PortfolioContainer/Home/WaveFolder/Wave"; 
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Profile />
+      <Wave />
+      <About />
+      <Experience />
+      <Contact />
       <Footer />
     </div>
   );
